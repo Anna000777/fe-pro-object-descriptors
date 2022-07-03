1542a9adf9f8343f3902ead7c924e3fad1ca00db
Anna <kazanok777@gmail.com>
2022-07-03 10:33:52 +0300

    added homework 15

--- src/index.js
+++ src/index.js
@@ -8,14 +8,28 @@
  *
  * @returns string[]
  */
-export const getKeysByDescriptor = (object, descriptor) => {};
+export const getKeysByDescriptor = (object, descriptor) => {
+  const descriptors = Object.getOwnPropertyDescriptors(object);
+  const keys = Object.entries(descriptors)
+    .filter((item) => item[1][descriptor] === true)
+    .map((item) => item[0]);
+  return keys;
+};
 
 /**
  * Должен вернуть true если объект был заморожен каким-либо методом заморозки freeze, seal, preventExtensions иначе false
  * @param {Object} object
  * @returns {boolean}
  */
-export const isObjectAnyFrozen = (object) => {};
+export const isObjectAnyFrozen = (object) => {
+  const frozen = Object.isFrozen(object);
+  const seal = Object.isSealed(object);
+  const extensible = Object.isExtensible(object);
+  if (frozen || seal || !extensible) {
+    return true;
+  }
+  return false;
+};
 
 /**
  * Принимает объект и строку. Мы должны вернуть НОВЫЙ объект(копию оригинального), в котором
@@ -27,7 +41,28 @@ export const isObjectAnyFrozen = (object) => {};
  *
  * @returns {Object}
  */
-export const assignLockedValues = (object, propertyName) => {};
+export const assignLockedValues = (object, propertyName) => {
+  const newObj = Object.create(
+    Object.getPrototypeOf(object),
+    Object.getOwnPropertyDescriptors(object)
+  );
+  if (!Object.keys(newObj).includes(propertyName)) {
+    Object.defineProperty(newObj, propertyName, {
+      value: null,
+      writable: false,
+      enumerable: true,
+      configurable: true,
+    });
+  } else {
+    Object.defineProperty(newObj, propertyName, {
+      value: object[propertyName],
+      writable: false,
+      enumerable: true,
+      configurable: true,
+    });
+  }
+  return newObj;
+};
 
 /**
  * Принимает объект и возвращает его копию, только абсолютно замороженную
@@ -35,4 +70,14 @@ export const assignLockedValues = (object, propertyName) => {};
  * @param {Object} object
  * @returns {Object}
  */
-export const freezeAllInObject = (object) => {};
+export const freezeAllInObject = (object) => {
+  const copiedObj = Object.create(
+    Object.getPrototypeOf(object),
+    Object.getOwnPropertyDescriptors(object)
+  );
+  // console.log(copiedObj);
+  // console.log(copiedObj === object);
+  //console.log(Object.getOwnPropertyDescriptors(object));
+
+  return Object.freeze(copiedObj);
+};
